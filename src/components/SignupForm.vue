ce13ba77a25d8cee0b8ef248c6717f4c29c5cc64
Amir Ekramian <amir.ekramian@samansalamat.ir>
2023-05-30 14:52:54 +0330

    Commited

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,234 @@
+<template>
+  <form @submit.self="HandleSubmit">
+    <label>Email:</label>
+    <input type="email" required v-model="email">
+
+    <label>Password:</label>
+    <div class="input-wrapper">
+
+    <input @click="viewcondition" @input="checkpasswordLength(); checkPasswordNumber(); checkPasswordCharacter(); CheckCapitalCharacter();" class="pass" :type="Passtype" required v-model="password">
+    <FontAwesomeIcon @click="ChangePasswordVisibility" v-if="!PasswordVisibility" class="Icon" icon="eye" />
+    <FontAwesomeIcon @click="ChangePasswordVisibility" v-if="PasswordVisibility" class="Icon" icon="eye-slash" />
+    </div>
+    <div v-if="PassCondition">
+    <div :class="{acceptedCondition:lengthCondition}" class="pill">At least8 digit</div>
+    <div :class="{acceptedCondition:numberCondition}" class="pill">Number</div>
+    <div :class="{acceptedCondition:charCondition}" class="pill">Special character</div>
+    <div :class="{acceptedCondition:capitalCondition}" class="pill">Capital character</div>
+
+    </div>
+    <br>
+    <label>Role:</label>
+    <select v-model="role">
+        <option value="DevopsEngineer">Devops Engineer</option>
+        <option value="Developer">Developer</option>
+        <option value="CTO">CTO</option>
+         <option value="TeamLead">Team Lead</option>
+    </select>
+    <label>Skills</label>
+    <input type="text" v-model="tempSkill" @keyup="AddSkill" placeholder="Use , for insert your skill">   
+    <div v-for="Skill in Skills" :key="Skill" class="pill">
+        <div>
+        {{Skill}}
+        <button class="DelBtn" @click="RemoveSkill(Skill)">
+            <img src="/src/assets/icons8-delete-48.png" alt="X">
+        </button>
+        </div>        
+    </div>
+    <div class="terms">
+        <input type="checkbox" required v-model="terms">
+        <label>Accept terms of service</label>
+    </div>
+    <div>
+        <div class="submit">
+        <button class="submitBTN">
+         submit
+        </button>   
+        <button class="btn btn-default">
+         cancel
+        </button>       
+        </div>
+    </div>
+  </form>
+  {{password}}
+</template>
+
+<script>
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faEye);
+library.add(faEyeSlash);
+
+
+
+export default {
+data(){
+    return{
+        email:'',
+        password:'',
+        role:'',
+        terms: false,
+        tempSkill:'',
+        Skills:[],
+        PassCondition:false,
+        lengthCondition:false,
+        charCondition: false,
+        numberCondition: false,
+        capitalCondition:false,
+        PasswordVisibility:false,
+        Passtype:'password'
+    }
+},
+components:{
+FontAwesomeIcon
+},
+methods:{
+    AddSkill(e){
+        var temporary = this.tempSkill.replace(',','').toLowerCase()
+        if(e.key === ',' && temporary){
+            if(!this.Skills.includes(temporary)){
+            this.Skills.push(temporary)
+            }
+            this.tempSkill = ''
+        }
+    },
+    RemoveSkill(Skill){
+        this.Skills.splice(this.Skills.indexOf(Skill),1)
+    },
+    HandleSubmit(){
+        console.log('Form submited...')
+    },
+    viewcondition(){
+        this.PassCondition = true
+    },
+    checkpasswordLength(){
+        if(this.password.length >=8){
+            this.lengthCondition =true
+        }
+        else{this.lengthCondition =false}
+    },
+    checkPasswordNumber(){
+            var format = /[0-9]+/
+            var result = format.test(this.password)
+        if(result){
+            this.numberCondition =true            
+            }
+            else{this.numberCondition = false}
+    },
+     checkPasswordCharacter(){
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
+    var result = format.test(this.password)
+         if(result){
+            this.charCondition = true
+            }
+            else{this.charCondition = false}
+     },
+     CheckCapitalCharacter(){
+         var format = /[A-Z]+/
+         var result = format.test(this.password)
+         if(result){
+        this.capitalCondition = true
+         }else{this.capitalCondition = false}
+     },
+     ChangePasswordVisibility(){
+        this.PasswordVisibility =!this.PasswordVisibility
+        if(this.PasswordVisibility){
+            this.Passtype='text'
+        }
+        else{
+            this.Passtype='password'
+        }
+     }
+}
+}
+</script>
+
+<style scoped>
+form {
+max-width: 420px;
+margin: 30px auto;
+background:white;
+text-align: left;
+padding: 40px;
+border-radius: 10px;
+}
+label {
+color: #aaa;
+display: inline-block;
+margin: 25px 0 15px;
+font-size: 0.6em;
+text-transform: uppercase;
+letter-spacing: 1px;
+font-weight: bold;
+}
+input,select {
+display: block;
+padding: 10px 6px;
+width: 100%;
+box-sizing: border-box;
+border: none;
+border-bottom: 1px solid #302323;
+color: #555;
+}
+
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin:0 10px 0 0;
+    position: relative;
+    top: 2px;
+    padding-right: 30px;
+}
+.pill{
+    display: inline-block;
+    font-size: 0.7em;
+    padding: 0 5px 0 5px;
+    background: #e0e0e0;
+    margin: 3px 3px;
+    border-radius: 100px;
+}
+.DelBtn{
+    color: #b90101;
+    border-style: none;
+    background: transparent;
+}
+.submitBTN{
+    background: #5002aa;
+    color: white;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 10px;
+    margin-right: 10px;
+    border-radius: 15px;
+    text-align: center;
+    display: inline-block ;
+}
+div.submit{
+    text-align: center;
+}
+.acceptedCondition{
+    display: inline-block;
+    font-size: 0.7em;
+    padding: 0 5px 0 5px;
+    background: #16b156;
+    color:white;
+    margin: 3px 3px;
+    border-radius: 100px;
+}
+.Icon{
+     position: absolute;
+     top: 50%;
+     right: 5px;
+     transform: translateY(-50%);
+}
+.input-wrapper{
+      position: relative;
+
+}
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/vue-fontawesome'
+
+createApp(App).mount('#app')
